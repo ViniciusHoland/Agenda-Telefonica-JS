ad1a86536887efe06338d9270023a951d6bd3070
ViniciusDev <viniciusdev10@gmail.com>
2024-12-26 17:29:02 -0300

    created endpoint delete contact by Id

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ app.listen(3000)
 // informa ao express que vamos usar json nas requisições
 app.use(express.json())
 
-
-const listContacts = []
 let nextID = 1
+const listContacts = [
+    {name: 'vinicius', contact: 94723569, id: nextID++},
+    {name: 'vitoria', contact: 80800404, id: nextID++}
+
+]
+
 
 app.post('/contacts', (request, response) => {
 
@@ -39,6 +43,23 @@ app.get('/contacts/:id', (req , res ) => {
 
     res.status(200).json(listContacts[id-1])
 
+
+})
+
+app.delete('/contacts/:id', (req,res) => {
+
+    const id = req.params.id 
+
+    const indexToRemove = listContacts.findIndex(contact => contact.id == id)
+    
+
+    if (indexToRemove === -1) {
+        return res.status(404).json({ error: 'Contact not found' });
+    }
+
+    const removedContact =  listContacts.splice(indexToRemove,1)
+
+    res.status(200).json(removedContact)
 
 })
 
